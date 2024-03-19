57d9e4a649a11c7122f96a1caeb91262b9ea6179
Dumitru C <canteadumitru@gmail.com>
2024-03-19 07:05:01 +0200

    Update contact-section.tsx

--- app/components/contact-section.tsx
+++ app/components/contact-section.tsx
@@ -43,7 +43,7 @@ export default function ContactSection() {
               className="flex rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
               <MessengerIcon className="h-5 w-5" />
-              <span className="ml-2">Messanger</span>
+              <span className="ml-2">Messenger</span>
             </a>
           </div>
         </div>
